4d11c093ad3212c70ea638b01894d9daf9f1fb41
Rafael Junior <Rafaeljunior13777@gmail.com>
2026-09-03 23:48:49 -0300

    Dia 3/30

---
+++ Dia_03/aula_07.py
@@ -0,0 +1,9 @@
+# nome_completo = 'Rafael Junior de Souza'
+# soma_dois_mais_dois = 2 + 2
+# print(soma_dois_mais_dois)
+# print(nome_completo)
+
+nome = 'Rafael'
+idade = 17
+maior_de_idade = idade >= 18
+print('Nome:', nome, 'Idade:', idade, 'É maior de idade?', maior_de_idade)
