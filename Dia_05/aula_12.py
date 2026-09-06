72ec5c116cdce177839e56da93d056a4c1e03836
Rafael Junior <Rafaeljunior13777@gmail.com>
2026-09-05 22:56:18 -0300

    Dia 5/30

---
+++ Dia_05/aula_12.py
@@ -0,0 +1,6 @@
+nome = "Rafael Junior"
+altura = 1.78
+peso = 80
+
+resultado_imc = peso / altura ** 2
+print(resultado_imc)
---
+++ Dia_05/aula_13.py
@@ -0,0 +1,11 @@
+nome = "Rafael Junior"
+altura = 1.78
+peso = 80
+imc = peso / altura ** 2
+
+"f-strings"
+linha_1 = f'{nome} tem {altura:.2f} de altura'
+linha_2 = f'pesa {peso} quilos e seu imc é {imc:.2f}'
+
+print(linha_1)
+print(linha_2)
