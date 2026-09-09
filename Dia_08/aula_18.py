740d70d82ce9f13bd76f93de1bff5411e179252b
Rafael Junior <Rafaeljunior13777@gmail.com>
2026-09-08 23:05:42 -0300

    Dia 8/30

---
+++ Dia_08/aula_18.py
@@ -0,0 +1,19 @@
+condicao1 = True
+condicao2 = True
+condicao3 = True
+condicao4 = True
+
+if condicao1:
+    print("Condição 1 é verdadeira")
+    print("Condição 1 é verdadeira")
+elif condicao2:
+    print("Condição 2 é verdadeira")
+elif condicao3:
+    print("Condição 3 é verdadeira")
+elif condicao4:
+    print("Condição 4 é verdadeira")
+else:
+    print("Nenhuma condição é verdadeira")                
+
+if 10 == 10:
+    print("10 é igual a 10")    
