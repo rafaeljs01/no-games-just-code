5315774b323e54cd9c61509c0f8e717715426969
Rafael Junior <Rafaeljunior13777@gmail.com>
2026-09-12 22:54:23 -0300

    Dia 12/30

---
+++ Dia_12/aula_23.py
@@ -0,0 +1,11 @@
+senha = input("Digite a senha: ")
+
+if senha != "123456":
+    print("Acesso negado!")    
+else:
+    print("Acesso permitido!")
+
+print(not 1)    
+print(not 0)
+print(not True)
+print(not False)
