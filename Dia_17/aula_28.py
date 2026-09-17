ae96136b694d2fcb5aa2fcaa36d5eabedf0501bd
Rafael Junior <Rafaeljunior13777@gmail.com>
2026-09-17 20:02:51 -0300

    Dia 17/30

---
+++ Dia_17/aula_28.py
@@ -0,0 +1,18 @@
+nome = input('Digite o seu nome: ')
+idade = input('Digite sua idade: ')
+
+if nome and idade:
+    print(f'Seu nome é {nome}')
+    print(f'Seu nome invertido é {nome[::-1]}')
+
+    if ' ' in nome:
+        print('Seu nome contém espaços')
+    else:
+        print('Seu nome não contem espaços')
+
+
+    print(f'Seu nome tem {len(nome)} letras')
+    print(f'A primeira letra do seu nome é  {nome[0]}')
+    print(f'A ultima letra do seu nome é  {nome[-1]}')
+else:
+    print('Desculpe, voce deixou campos vazios')    
