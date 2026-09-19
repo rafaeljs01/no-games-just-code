80df93ce0bc082acfe665b22a79c3af0a4d38a4f
Rafael Junior <Rafaeljunior13777@gmail.com>
2026-09-18 23:14:17 -0300

    Dia 18/30

---
+++ Dia_18/aula_29.py
@@ -0,0 +1,20 @@
+# try
+#except
+
+
+# print(1234)
+# print(43543)
+# int('a')
+
+
+
+
+numero_str = input('Vou dobrar o número que você digitar: ')
+
+try:
+    print('STR:', numero_str)
+    numero_float = float(numero_str)
+    print('FLOAT:', numero_float)
+    print(f'O dobro de {numero_str} é {numero_float * 2:.2f}')
+except:
+    print('Isso não é um número válido!')
