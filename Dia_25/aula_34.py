f736334bef75693f4b266202c2088a64bf52ea04
Rafael Junior <Rafaeljunior13777@gmail.com>
2026-09-25 23:54:44 -0300

    Dia 25/30

---
+++ Dia_25/aula_34.py
@@ -0,0 +1,12 @@
+#REPETIÇÕES
+# While
+
+condicao = True
+while condicao:
+    nome = input("Digite seu nome: ")
+    print(f'Seu nome é {nome}')
+
+    if nome == 'sair':
+        break
+
+print(123)    
